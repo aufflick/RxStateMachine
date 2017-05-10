b9ff973692dc837fc855ef479e0ebb6b564e2cdb
Mark Aufflick <mark@aufflick.com>
2017-05-10 12:48:47 +1000

    Initial Commit

---
+++ RxStateMachine/RxStateMachine.h
@@ -0,0 +1,19 @@
+//
+//  RxStateMachine.h
+//  RxStateMachine
+//
+//  Created by Mark Aufflick on 10/5/17.
+//  Copyright © 2017 The High Technology Bureau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxStateMachine.
+FOUNDATION_EXPORT double RxStateMachineVersionNumber;
+
+//! Project version string for RxStateMachine.
+FOUNDATION_EXPORT const unsigned char RxStateMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxStateMachine/PublicHeader.h>
+
+
